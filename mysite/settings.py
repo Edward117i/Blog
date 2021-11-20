7639d3fba2a96773b19835526ed20dae9f71c835
Edwardi117 <sampatbm3@gmail.com>
2021-11-20 09:39:54 -0600

    Form rendering

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'JEdwardCS117@gmail.com'
+EMAIL_HOST_PASSWORD = 'Tworks117'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
